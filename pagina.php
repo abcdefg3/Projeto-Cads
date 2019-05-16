e3d6ff9a8f672877f846aa99cea1f848eefa2945
Willian Belizario <wbdn123456789@gmail.com>
2019-05-16 02:03:24 -0300

    m commit

--- pagina.php
+++ pagina.php
@@ -1,14 +1,6 @@
 <?php
-$n=$_GET["w"];
-
-echo "O Preço é de:".number_format($n, 2);
-
-$n += $n*30/100;
-
-echo "<br/>"."O Resultado da procentagem é :".number_format($n, 2);
-
-$a=3;
-$b=&$a;
-$b= 5;
-echo "<br> $a"
+$nome=$_GET["nome"];
+$sobrenome=$_GET["sobrenome"];
+echo $nome.$sobrenome;
+echo "<br>Você cadastro a Parada";
 ?>
