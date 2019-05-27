7d7c58d30ffa8cf9bdacf29a2cc93b93110f6b2c
Willian Belizario <wbdn123456789@gmail.com>
2019-05-27 02:29:45 -0300

    trabalho de php

--- pagina.php
+++ pagina.php
@@ -1,6 +1,9 @@
 <?php
 $nome=$_GET["nome"];
 $sobrenome=$_GET["sobrenome"];
+$raiz=$_GET["raiz"];
+$skol=sqrt($raiz);
 echo $nome.$sobrenome;
+echo "A Raiz quadrada de $raiz é:".number_format($skol,2);
 echo "<br>Você cadastro a Parada";
 ?>
